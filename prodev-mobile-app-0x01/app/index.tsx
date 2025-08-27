fba8a56f8c13711eb67a63fb87faddd6a866fe21
loicelaura <loicelaura@gmail.com>
2025-08-27 17:32:24 +0300

    updated index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+      <Text style={styles.largeText}>
+        Typescript is great if you practice more
+      </Text>
+      <Text style={styles.mediumText}>
+        React Native provides you a single codebase for cross platforms
+      </Text>
       <Text style={styles.smallText}>ALX is awesome</Text>
+
+      <Text>Entry Screen - Awesome</Text>
     </View>
   );
 }
@@ -13,18 +19,18 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   largeText: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   mediumText: {
     fontSize: 18,
   },
   smallText: {
     fontSize: 14,
-    fontStyle: "italic",
+    fontStyle: 'italic',
   },
 });
